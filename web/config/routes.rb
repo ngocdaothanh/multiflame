2a8f16dd881ab8fe00a0c98c83496a9893cd9ec4
ngocdaothanh <ngocdaothanh@4329d0ce-1545-0410-b4e1-19340f1d7e97>
2008-03-13 13:50:51 +0000

    Move CAPTCHA and mail utility from web to fifo

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -19,7 +19,7 @@ ActionController::Routing::Routes.draw do |map|
   map.play_lang 'games/:id/:channel/:lang', :controller => 'games', :action => 'play'
 
   map.resources :games
-  map.resources :toys, :collection => {:captcha => :get, :mail => :post}
+  map.resources :toys
 
   map.connect 'admin', :controller => 'admin/stats'
   map.namespace :admin do |admin|
