97baa019237d298530ccbe4f50f4eed890ad49b6
ngocdaothanh <ngocdaothanh@4329d0ce-1545-0410-b4e1-19340f1d7e97>
2008-05-23 00:02:29 +0000

    Use JRuby

--- wserver/config/environment.rb
+++ wserver/config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # To use Rails without a database, you must remove the Active Record framework
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -34,10 +34,10 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
+  # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_rails2_session',
+    :session_key => '_session_id',
     :secret      => '62b0b7749c802b1b4d852e5518189d0c189dc2a0ce5fc4f236cbab652896684b0a69aec06fca46c6e8f9fdd656dfa2950fdc3efb05eddfa1fb7823d651cab8fe'
   }
 
@@ -63,11 +63,3 @@ end
 $KCODE = 'u'
 require 'jcode'
 require 'gettext/rails'
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :port           => 587,
-  :user_name      => 'web20games@gmail.com',
-  :password       => 'k@w@111f',
-  :authentication => :plain
-}
