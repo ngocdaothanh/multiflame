233454ac5f592dd0448d5ef4cceb9752a898c6cd
ngocdaothanh <ngocdaothanh@4329d0ce-1545-0410-b4e1-19340f1d7e97>
2008-10-22 16:54:41 +0000

    Finish AMF3 codec

--- gserver/src/amf3.erl
+++ gserver/src/amf3.erl
@@ -3,7 +3,7 @@
 % * false, true
 % * integer, number
 % * string
-% * dense array
+% * dense array, byte array
 %
 % More information:
 % * Official AMF3 spec: http://download.macromedia.com/pub/labs/amf/amf3_spec_121207.pdf
@@ -18,10 +18,13 @@
 -define(FALSE,       2).
 -define(TRUE,        3).
 -define(INTEGER,     4).
--define(NUMBER,      5).
+-define(DOUBLE,      5).
 -define(STRING,      6).
 -define(ARRAY,       9).
 -define(BYTE_ARRAY, 12).
+
+-define(INTEGER_MIN, -268435456).
+-define(INTEGER_MAX,  268435455).
 
 %-------------------------------------------------------------------------------
 decode_all(Data) ->
@@ -49,9 +52,9 @@ decode(<<?TRUE, Rest/binary>>) ->
 decode(<<?INTEGER, Rest/binary>>) ->
     decode_integer(Rest);
 
-decode(<<?NUMBER, Rest/binary>>) ->
-    <<Float:64/float, Rest2/binary>> = Rest,
-    {Float, Rest2};
+decode(<<?DOUBLE, Rest/binary>>) ->
+    <<Double:64/float, Rest2/binary>> = Rest,
+    {Double, Rest2};
 
 decode(<<?STRING, Rest/binary>>) ->
     {Type, Rest2} = decode_integer(Rest),
@@ -62,7 +65,13 @@ decode(<<?STRING, Rest/binary>>) ->
 decode(<<?ARRAY, Rest/binary>>) ->
     {Type, Rest2} = decode_integer(Rest),
     Length = Type bsr 1,
-    decode_array(Length, Rest2).
+    decode_array(Length, Rest2);
+
+decode(<<?BYTE_ARRAY, Rest/binary>>) ->
+    {Type, Rest2} = decode_integer(Rest),
+    Length = Type bsr 1,
+    <<Binary:Length/binary, Rest3/binary>> = Rest2,
+    {Binary, Rest3}.
 
 %-------------------------------------------------------------------------------
 decode_integer(Data) ->
@@ -95,5 +104,44 @@ decode_array(Length, Data, Acc) ->
     decode_array(Length - 1, Rest, [Item | Acc]).
 
 %-------------------------------------------------------------------------------
-encode(Variable) ->
-    ok.
+encode(undefined) ->
+    <<?UNDEFINED>>;
+
+encode(null) ->
+    <<?NULL>>;
+
+encode(false) ->
+    <<?FALSE>>;
+
+encode(true) ->
+    <<?TRUE>>;
+
+encode(Integer) when is_integer(Integer), ?INTEGER_MIN =< Integer, Integer =< ?INTEGER_MAX ->
+    <<?INTEGER, (encode_integer(Integer))/binary>>;
+
+encode(Double) when is_number(Double) ->
+    <<?DOUBLE, Double:64/float>>;
+
+encode(List) when is_list(List) ->
+    Length = length(List),
+    List2 = lists:map(fun(X) -> encode(X) end, List),
+    Binary = lists:foldl(fun(X, Acc) -> <<Acc/binary, X/binary>> end, <<>>, List2),
+    <<?ARRAY, (encode_integer(Length bsl 1 bor 1))/binary, 1, Binary/binary>>;
+
+encode(Binary) when is_binary(Binary) ->
+     Length = length(binary_to_list(Binary)),
+     <<?BYTE_ARRAY, (encode_integer(Length bsl 1 bor 1))/binary, Binary/binary>>.
+
+%-------------------------------------------------------------------------------
+encode_integer(Integer) ->
+    Int = Integer band 16#1fffffff,
+    if
+        Int < 16#80 ->
+            <<Int>>;
+        Int < 16#4000 ->
+            <<(Int bsr 7 band 16#7f bor 16#80), (Int band 16#7f)>>;
+        Int < 16#200000 ->
+            <<(Int bsr 14 band 16#7f bor 16#80), (Int bsr 7 band 16#7f bor 16#80), (Int band 16#7f)>>;
+        true ->
+            <<(Int bsr 22 band 16#7f bor 16#80), (Int bsr 15 band 16#7f bor 16#80), (Int bsr 8 band 16#7f bor 16#80), (Int band 16#ff)>>
+    end.
