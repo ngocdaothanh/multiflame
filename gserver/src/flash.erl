3843598a1c90ca34ec24b849f7c1b7793a5d9e33
ngocdaothanh <ngocdaothanh@4329d0ce-1545-0410-b4e1-19340f1d7e97>
2008-10-23 19:31:29 +0000

    Restructure

--- gserver/src/flash.erl
+++ gserver/src/flash.erl
@@ -1,25 +1,24 @@
--module(gserver_tcp).
--export([start/1]).
+-module(flash).
+-export([start_listener/1]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
-start(Port) ->
+start_listener(Port) ->
     {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-    accept(LSocket, []).
+    accept(LSocket).
 
-accept(LSocket, Sockets) ->
+accept(LSocket) ->
     {ok, Socket} = gen_tcp:accept(LSocket),
-    spawn(fun() -> recv(Socket, self()) end),
-    accept(LSocket, [Socket | Sockets]).
+    spawn(fun() -> proxy(Socket) end),
+    accept(LSocket).
 
-recv(Socket, ParentPid) ->
+proxy(Socket) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Data} ->
             %gen_tcp:send(Socket, Data),
             io:format("~p~n", [Data]),
             io:format("~p~n", [amf3:decode_all(Data)]),
-            recv(Socket, ParentPid);
+            proxy(Socket);
         {error, closed} ->
-            gen_server:call(gserver_server, {closed, Socket}),
             ok
     end.
